5b99eb58356798fc0583cda40aaeb4f5fbcdf26a
ezeob002 <ezeob002@unm.edu>
2017-08-08 10:54:34 -0400

    Initial Commit

---
+++ PokeSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeSearch
+//
+//  Created by Uchenna Ezeobi on 8/8/17.
+//  Copyright © 2017 Uchenna Ezeobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
